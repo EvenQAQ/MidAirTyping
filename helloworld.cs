9f5a63a4571072e15feef0b4037bcc821468c89d
尹思程 <yinsicheng1999@outlook.com>
2019-05-12 23:43:49 +0800

    12

--- helloworld.cs
+++ helloworld.cs
@@ -22,13 +22,38 @@ class mylistener : Listener
     public override void OnFrame (Controller controller)
     {
         Frame frame = controller.Frame();
+        HandList hands = frame.Hands;
+        Hand rhand, lhand;
+        if (hands[0].IsRight)
+        {
+            lhand = hands[1];
+            rhand = hands[0];
+        }
+        else
+        {
+            lhand = hands[0];
+            rhand = hands[1];
+        } 
+        Finger lindex = lhand.Fingers[1], rindex = rhand.Fingers[1];
         SafeWriteLine ("Frame available");
-        SafeWriteLine ("Frame id: " + frame.Id
-             + ", timestamp: " + frame.Timestamp
-             + ", hands: " + frame.Hands.Count
-             + ", fingers: " + frame.Fingers.Count
-             + ", tools: " + frame.Tools.Count
-             + ", gestures: " + frame.Gestures ().Count);
+        // SafeWriteLine ("Frame id: " + frame.Id
+        //      + ", timestamp: " + frame.Timestamp
+        //      + ", hands: " + frame.Hands.Count
+        //      + ", fingers: " + frame.Fingers.Count);
+
+        SafeWriteLine("lhand: " + lhand.PalmPosition
+             + ", lindex: " + lindex.ToString() + lindex.Type
+             + ", tippos: " + lindex.TipPosition
+             + ", stapos: " + lindex.StabilizedTipPosition
+             + ", rhand: " + rhand.PalmPosition
+             + ", rindex: " + rindex.ToString() + rindex.Type
+             + ", tippos: " + rindex.TipPosition
+             + ", stapos: " + rindex.StabilizedTipPosition);
+
+        // foreach (Finger f in lfingers)
+        // {
+        //     Console.WriteLine(f.Type);
+        // }
     }
 
 }
